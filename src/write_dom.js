982b0916b9975bbc16d70bd895064b91eecd89c6
Jay Adkisson <jjmadkisson@gmail.com>
2012-06-19 10:57:34 -0700

    style fix

--- src/write_dom.js
+++ src/write_dom.js
@@ -25,9 +25,11 @@ function writeDOM(el) {
 
     buffer.push(str);
 
-    if (block) block.eachChild(function(child) {
-      child.writeHTML(write);
-    });
+    if (block) {
+      block.eachChild(function(child) {
+        child.writeHTML(write);
+      });
+    }
   }
 
   function register(el) {
